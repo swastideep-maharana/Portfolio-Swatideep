1cce344a4cd3bb3788d084254c65203a1c427e84
swastideep maharana <swastideep67maharana@gmail.com>
2025-06-20 13:16:00 +0530

    #added metadata fixes

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -24,9 +24,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     };
   } else {
     return {
-      title: "Projects | John Doe",
+      title: "Projects | Swastideep",
       description:
-        "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+        "Swastideep is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
     };
   }
 }
